9be33f528201ae11573493b0078a350389ac19f7
ikegdivs <ikeg@pjdivs.com>
2021-05-04 15:35:40 -0700

    File upload

---
+++ src/library.js
@@ -0,0 +1,9 @@
+// Emscripten functions
+mergeInto(LibraryManager.library, {
+    setTMV: function() {
+        console.log("hey doot");
+            //alert('helli');
+            //var TMV = newTMV;
+        }
+    }
+)
